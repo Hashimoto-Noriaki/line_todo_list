a592b17758a3288c940a06f318024365c186dc23
橋本範昭 <nori.hasimo.89@gmail.com>
2021-02-13 19:37:24 +0900

    line_bot_controllerを実装してLINEと接続準備

--- app/controllers/line_bot_controller.rb
+++ app/controllers/line_bot_controller.rb
@@ -1,15 +1,41 @@
 class LineBotController < ApplicationController
-  # protect_from_forgery with: :null_session
+  require "line/bot"
+
+  protect_from_forgery with: :null_session
 
   def callback
+    #LINEで送られてきたメッセージデータ取得
+    body = request.body.read
     binding.pry
+
+    #LINE以外からリクエストがきた場合エラーを返す
+    signature = request.env["HTTP_X_LINE_SIGANATURE"]
+    unless client.validate_signature(body,signature)
+      head :bad_request and return
+      binding.pry
+  end
+  #LINEで送られたメッセージを適切な形式に変形
+  events = client.parse＿events_from(body)
+
+  events.each do |event|
+    #LINEからテキストが送信された時
+    if (event.type === Line::Bot::Event::MessageType::Text)
+      #Lineからテキストが送信された時の処理を記述
+    end
   end
 
-  def show
-    
-  end
-
-  def create
-
-  end
+  #LINEのwebhookAPIとの連携をするためにstatus code200を返す
+  render json: {status: :ok}
+  binding.pry
 end
+
+private
+
+def client
+  @client ||= Line::Bot::Client.new do |config|
+    config.channel_secret = ENV["LINE_CHANNEL_SECRET"]
+    config.channel_token =  ENV["LINE_CHANNEL_TOKEN"]
+end
+end
+end
+
